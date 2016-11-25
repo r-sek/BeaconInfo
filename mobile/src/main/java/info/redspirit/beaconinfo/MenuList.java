7574a2da42eb3b8a22867449a4cc3b8b68d021fe
redspirit-rj <rs960222@gmail.com>
2016-11-25 10:57:12 +0900

    age

---
+++ mobile/src/main/java/info/redspirit/beaconinfo/MenuList.java
@@ -0,0 +1,27 @@
+package info.redspirit.beaconinfo;
+
+import android.graphics.Bitmap;
+
+/**
+ * Created by rj on 2016/11/18.
+ */
+public class MenuList {
+    private Bitmap icon;
+    private String menu;
+
+    public Bitmap getIcon() {
+        return icon;
+    }
+
+    public void setIcon(Bitmap icon) {
+        this.icon = icon;
+    }
+
+    public String getMenu() {
+        return menu;
+    }
+
+    public void setMenu(String menu) {
+        this.menu = menu;
+    }
+}
---
+++ wear/src/main/java/info/redspirit/beaconinfo/MyStubBroadcastActivity.java
@@ -0,0 +1,21 @@
+package info.redspirit.beaconinfo;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+/**
+ * Example shell activity which simply broadcasts to our receiver and exits.
+ */
+public class MyStubBroadcastActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Intent i = new Intent();
+        i.setAction("info.redspirit.beaconinfo.SHOW_NOTIFICATION");
+        i.putExtra(MyPostNotificationReceiver.CONTENT_KEY, getString(R.string.title));
+        sendBroadcast(i);
+        finish();
+    }
+}
