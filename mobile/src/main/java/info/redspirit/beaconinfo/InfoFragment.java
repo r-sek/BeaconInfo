261fcefad2af67fcb907c2a375bfbdd7df9b68c3
redspirit-rj <rs960222@gmail.com>
2016-11-30 17:05:55 +0900

    メモリリーク対策

--- mobile/src/main/java/info/redspirit/beaconinfo/InfoFragment.java
+++ mobile/src/main/java/info/redspirit/beaconinfo/InfoFragment.java
@@ -1,12 +1,17 @@
 package info.redspirit.beaconinfo;
 
 import android.content.Context;
+import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.media.Image;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 
 /**
@@ -64,7 +69,12 @@ public class InfoFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_info, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_info, container, false);
+
+        ImageView imageView = (ImageView)rootView.findViewById(R.id.mainImage);
+        imageView.setImageBitmap(decodeSampledBitmapFromResource(getResources(),R.drawable.sampleimg,330,220));
+
+        return rootView;
     }
 
     // TODO: Rename method, update argument and hook method into UI event
@@ -105,4 +115,40 @@ public class InfoFragment extends Fragment {
         // TODO: Update argument type and name
         void onFragmentInteraction(Uri uri);
     }
+
+    public static Bitmap decodeSampledBitmapFromResource(Resources res, int resId, int reqWidth, int reqHeight) {
+        /**縮小した画像の読み込み*/
+
+        final BitmapFactory.Options options = new BitmapFactory.Options();
+        options.inJustDecodeBounds = true;
+        BitmapFactory.decodeResource(res, resId, options);
+
+        options.inSampleSize = calculateInSampleSize(options, reqWidth, reqHeight);
+
+        options.inJustDecodeBounds = false;
+        return BitmapFactory.decodeResource(res, resId, options);
+    }
+
+    public static int calculateInSampleSize(BitmapFactory.Options options,int reqWidth,int reqHeight){
+        /**画像サイズの設定*/
+
+        final int width = options.outWidth;
+        final int height = options.outHeight;
+        int inSampleSize =1;
+
+        if(height > reqHeight || width > reqWidth){
+            final int halfWidth = width/2;
+            final int halfHeight = height/2;
+
+            while ((halfHeight / inSampleSize) > reqHeight && (halfWidth / inSampleSize) > reqWidth) {
+                inSampleSize *= 2;
+            }
+
+        }
+
+        return inSampleSize;
+
+    }
+
+
 }
