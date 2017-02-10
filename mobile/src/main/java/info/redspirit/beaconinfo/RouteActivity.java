0db61bef9f0538e40374f92770eb49669929040f
redspirit-rj <rs960222@gmail.com>
2017-02-10 14:30:57 +0900

    ルート案内がんばるぞい

---
+++ mobile/src/main/java/info/redspirit/beaconinfo/RouteActivity.java
@@ -0,0 +1,144 @@
+package info.redspirit.beaconinfo;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.LocationSource;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class RouteActivity extends AppCompatActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+
+    private GoogleApiClient mGoogleApiClient;
+    private LocationRequest locationRequest;
+    private LocationSource.OnLocationChangedListener onLocationChangedListener = null;
+
+    private int priority[] = {LocationRequest.PRIORITY_HIGH_ACCURACY, LocationRequest.PRIORITY_BALANCED_POWER_ACCURACY,
+            LocationRequest.PRIORITY_LOW_POWER, LocationRequest.PRIORITY_NO_POWER};
+    private int locationPriority;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_route);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+// LocationRequest を生成して精度、インターバルを設定
+        locationRequest = LocationRequest.create();
+
+        // 測位の精度、消費電力の優先度
+        locationPriority = priority[1];
+
+        if (locationPriority == priority[0]) {
+            // 位置情報の精度を優先する場合
+            locationRequest.setPriority(locationPriority);
+            locationRequest.setInterval(5000);
+            locationRequest.setFastestInterval(16);
+        } else if (locationPriority == priority[1]) {
+            // 消費電力を考慮する場合
+            locationRequest.setPriority(locationPriority);
+            locationRequest.setInterval(60000);
+            locationRequest.setFastestInterval(16);
+        } else if (locationPriority == priority[2]) {
+            // "city" level accuracy
+            locationRequest.setPriority(locationPriority);
+        } else {
+            // 外部からのトリガーでの測位のみ
+            locationRequest.setPriority(locationPriority);
+        }
+
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    // onResumeフェーズに入ったら接続
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mGoogleApiClient.connect();
+    }
+
+    // onPauseで切断
+    @Override
+    public void onPause() {
+        super.onPause();
+        mGoogleApiClient.disconnect();
+    }
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+
+    /*
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        // check permission
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED
+                || ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            Log.d("debug", "permission granted");
+
+            mMap = googleMap;
+            // default の LocationSource から自前のsourceに変更する
+            mMap.setLocationSource(this);
+            mMap.setMyLocationEnabled(true);
+            mMap.setOnMyLocationButtonClickListener(this);
+        }
+        else{
+            Log.d("debug", "permission error");
+            return;
+        }
+    }
+    */
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        // Add a marker in Sydney and move the camera
+        LatLng sydney = new LatLng(-34, 151);
+        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+    }
+
+    /*
+    @Override
+    public void onLocationChanged(Location location) {
+        Log.d("debug","onLocationChanged");
+        if (onLocationChangedListener != null) {
+            onLocationChangedListener.onLocationChanged(location);
+
+            double lat = location.getLatitude();
+            double lng = location.getLongitude();
+
+            Log.d("debug","location="+lat+","+lng);
+
+            Toast.makeText(this, "location="+lat+","+lng, Toast.LENGTH_SHORT).show();
+
+            // Add a marker and move the camera
+            LatLng newLocation = new LatLng(lat, lng);
+            mMap.addMarker(new MarkerOptions().position(newLocation).title("My Location"));
+            mMap.moveCamera(CameraUpdateFactory.newLatLng(newLocation));
+
+        }
+    }
+    */
+}
